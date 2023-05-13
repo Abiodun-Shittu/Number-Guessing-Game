d670f80dd5a4edde5e4576426a381565bcc6ca07
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-05-13 21:47:04 +0100

    Refactor code to use readline to get user input

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
 let gameLevel = 1;
 let gamePoint = 0;
 let minRange = 1;
 let maxRange = 2;
 let lives = 3;
-let randomNumber = Math.floor(Math.random() * maxRange + minRange);
+let randomNumber = generateRandomNumber();
 
-let userName = prompt("Hi, What is your name?");
+let userName = "";
 
-while (!userName) {
-	userName = prompt("Kindly Enter your name...");
+function askName() {
+	rl.question("Hi, What is your name? ", (answer) => {
+		if (answer.trim() === "") {
+			askName(); // Ask again if name is empty
+		} else {
+			userName = answer.trim();
+			game(); // Start the game
+		}
+	});
 }
 
-game();
+askName();
+
+function generateRandomNumber() {
+	return Math.floor(Math.random() * maxRange + minRange);
+}
 
 function game() {
 	greeting();
@@ -19,57 +37,67 @@ function game() {
 	answers();
 
 	function answers() {
-		let playerGuess = prompt(
-			`Guess a number between ${minRange} and ${maxRange}`
-		);
-
-		while (playerGuess !== randomNumber) {
-			if (lives <= 0) {
-				gameLost();
-				return;
-			} else if (Number.isNaN(parseInt(playerGuess))) {
-				playerGuess = prompt("Please enter a number");
-			} else if (playerGuess > randomNumber) {
-				lives--;
-				playerGuess = prompt(
-					"Guess too high, try guessing a lower number"
-				);
-			} else if (lives < randomNumber) {
-				lives--;
-				playerGuess = prompt(
-					"Guess too low, try guessing a higher number"
-				);
-			} else {
-				gameWon();
-				break;
+		rl.question(
+			`Guess a number between ${minRange} and ${maxRange}: `,
+			(playerGuess) => {
+				while (playerGuess !== randomNumber) {
+					if (lives <= 0) {
+						gameLost();
+						return;
+					} else if (Number.isNaN(parseInt(playerGuess))) {
+						rl.question("Please enter a number: ", (answer) => {
+							playerGuess = answer;
+						});
+					} else if (playerGuess > randomNumber) {
+						lives--;
+						rl.question(
+							"Guess too high, try guessing a lower number: ",
+							(answer) => {
+								playerGuess = answer;
+							}
+						);
+					} else if (playerGuess < randomNumber) {
+						lives--;
+						rl.question(
+							"Guess too low, try guessing a higher number: ",
+							(answer) => {
+								playerGuess = answer;
+							}
+						);
+					} else {
+						gameWon();
+						break;
+					}
+				}
 			}
-		}
+		);
 	}
 
 	function gameWon() {
 		console.log("Sweet!!, you guessed right\nReady to go again?\n");
-		prompt("Press enter to continue");
-		gameLevel++;
-		gamePoint++;
-		maxRange++;
-		randomNumber = Math.floor(Math.random() * maxRange + minRange);
-		lives = 3 + Math.floor(gameLevel / 4);
-		result();
-		answers();
+		rl.question("Press enter to continue", () => {
+			gameLevel++;
+			gamePoint++;
+			maxRange++;
+			randomNumber = generateRandomNumber();
+			lives = 3 + Math.floor(gameLevel / 4);
+			result();
+			answers();
+		});
 	}
 
 	function gameLost() {
 		console.log(
-			"Ooops... sorry, you ran out of lives\nNice game, you did well"
+			"Oops... sorry, you ran out of lives\nNice game, you did well"
 		);
 		result();
 		console.log("Thanks for playing");
-		return;
+		rl.close();
 	}
 
 	function result() {
 		console.table({
-			payer: userName,
+			player: userName,
 			level: gameLevel,
 			points: gamePoint,
 			lives: lives,
@@ -79,24 +107,28 @@ function game() {
 
 function greeting() {
 	console.log(`Hello ${userName}, You're welcome to my Number Guessing Game`);
-
-	prompt("Press Enter to continue...");
-
-	console.log(
-		"\nThis is a game where you guess the number I have in mind\nAnd for every streak of 3 guesses, you get an additional live\nYou get started with 3 lives...\n"
-	);
-	let playGame = prompt("Are you ready?\tType yes or no ");
-	while (playGame) {
-		if (playGame.toUpperCase() === "YES") {
-			console.log("Let's play..... \n");
-			break;
-		} else if (playGame.toUpperCase() === "NO") {
-			console.log("\nThank you and Goodbye!!!\n");
-			return;
-		} else {
-			playGame = prompt(
-				"Kindly type 'yes' to play game or 'no' if you wanna chicken out"
-			);
-		}
-	}
+	rl.question("Press Enter to continue...", () => {
+		console.log(
+			"\nThis is a game where you guess the number I have in mind\nAnd for every streak of 3 guesses, you get an additional live\nYou get started with 3 lives...\n"
+		);
+		rl.question("Are you ready?\tType yes or no: ", (answer) => {
+			while (answer) {
+				if (answer.toUpperCase() === "YES") {
+					console.log("Let's play..... \n");
+					break;
+				} else if (answer.toUpperCase() === "NO") {
+					console.log("\nThank you and Goodbye!!!\n");
+					rl.close();
+					return;
+				} else {
+					rl.question(
+						"Kindly type 'yes' to play game or 'no' if you wanna chicken out: ",
+						(answer) => {
+							answer = answer;
+						}
+					);
+				}
+			}
+		});
+	});
 }
